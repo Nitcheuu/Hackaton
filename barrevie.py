9ad9f20d705ea63eab3cfeaaab050981e16b36b9
Nitcheu <ph.favrelle@gmail.com>
2022-03-11 13:14:04 +0100

    Barre de vie

--- barrevie.py
+++ barrevie.py
@@ -10,7 +10,13 @@ class BarreVie:
         self.x_y = x_y
         self.rectangle_max = pg.Rect(x_y[0], x_y[1], self.longueur, 20)
         self.rectangle_actuel = pg.Rect(x_y[0], x_y[1], self.longueur, 20)
+        self.rectangle_fond = pg.Rect(x_y[0] - 10, x_y[1] - 70, self.longueur + 20, 100)
+        self.font = pg.font.SysFont("comicsansms", 20)
 
     def maj(self):
+        pg.draw.rect(self.ecran, (255, 255, 255), self.rectangle_fond)
+        self.ecran.blit(self.font.render(self.pokemon.nom, True, (0, 0, 0)), (self.x_y[0], self.x_y[1] - 60))
+        self.ecran.blit(self.font.render(f"{self.pokemon.statVieActuel} / {self.pokemon.statVie}", True, (0, 0, 0)), (self.x_y[0], self.x_y[1] - 35))
         pg.draw.rect(self.ecran, (255, 0, 0), self.rectangle_max)
         pg.draw.rect(self.ecran, (0, 255, 50), pg.Rect(self.x_y[0], self.x_y[1], (self.pokemon.statVie / self.pokemon.statVieActuel * self.longueur), 20))
+
--- combat.py
+++ combat.py
@@ -17,7 +17,9 @@ class Combat:
         # Définition de l'ennemi
         self.ennemi = Pokemon("", "", ennemi=True)
         # Définition barre de vie du joeuur
-        self.barre_joueur = BarreVie(self.joueur.pokemon_actif, (100, 300), ecran, 350)
+        self.barre_joueur = BarreVie(self.joueur.pokemon_actif, (20, 300), ecran, 350)
+
+        self.barre_ennemi = BarreVie(self.ennemi, (600, 80), ecran, 350)
 
 
 
@@ -28,4 +30,5 @@ class Combat:
         self.ecran.blit(self.joueur.pokemon_actif.sprite, (100, 310))
         self.ecran.blit(self.ennemi.sprite, (650, 140))
         self.barre_joueur.maj()
+        self.barre_ennemi.maj()
 
